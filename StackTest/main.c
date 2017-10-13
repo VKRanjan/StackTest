3fab7a37e42fe9a28739ddfbae18a52d9c609027
Vinay Kumar <vinay@supports-MacBook-Pro-2.local>
2017-10-13 11:23:33 +0530

    Initial Commit

---
+++ StackTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  StackTest
+//
+//  Created by Vinay Kumar on 10/13/17.
+//  Copyright © 2017 Vinay Kumar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
