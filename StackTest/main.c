fd9422dc8b75106fcbca009d7c4691e555b4bc7c
Vinay Nation <vinay.raj.apple@gmail.com>
2017-10-13 14:04:11 +0530

    Stack Test
    Demo

--- StackTest/main.c
+++ StackTest/main.c
@@ -7,9 +7,97 @@
 //
 
 #include <stdio.h>
+#define CAPACITY 5
+
+int stack[CAPACITY], top = -1;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int ch;
+    int item;
+    
+    
+    int isFull(void);
+    int isEmpty(void);
+    void pop(void);
+    void peek(void);
+    void traverse(void);
+    void quit(void);
+    void push(int ele);
+    
+    printf("1. Push \n");
+    printf("2. Pop \n");
+    printf("3. Peek \n");
+    printf("4. Traverse \n");
+    printf("5. Quit \n \n");
+    
+    printf("Enter your choice\n");
+    scanf("%d",&ch);
+    
+    
+    while (1) {
+        switch (ch) {
+            case 1:
+                printf("Enter element\n");
+                scanf("%d",&item);
+                push(item);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                peek();
+                break;
+            case 4:
+                traverse();
+                break;
+            case 5:
+                quit();
+                break;
+            default:
+                break;
+        }
+
+    }
+    
     return 0;
 }
+
+
+int isFull(){
+    if (top == CAPACITY -1) return 1; else return 0;
+}
+
+int isEmpty(){
+    
+    if (top == -1) return 1; else return 0;
+}
+
+void push(int ele){
+    
+    if (isFull()) {
+        printf("Stack is overflow");
+    }else{
+        top = top + 1;
+        stack[top] = ele;
+        printf("inserted element is %d",ele);
+    }
+}
+
+void pop(){
+    
+}
+
+void peek(){
+    
+}
+
+void traverse(){
+    
+}
+
+void quit(){
+    
+}
+
+
